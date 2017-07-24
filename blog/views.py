02d5cf815d5f02a0435507b9c3660fc1f8321d57
frank <franciskipchumba5@gmail.com>
2017-07-24 19:46:41 +0300

    list and detail vew

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,29 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+	posts = Post.published.all()
+	ctx = {
+		'posts':posts,
+	}
+	return render(request,
+				'blog/post/list.html',
+				ctx
+				)
+
+def post_detail(request, year, month, day, post):
+	post = get_object_or_404(Post, slug=post,
+									status='published',
+									publish__year = year,
+									publish__month=month,
+									publish__day=day)
+	ctx ={
+		'post':post,
+		}
+	return render(request,
+					'blog/post/detail.html',
+					ctx)
